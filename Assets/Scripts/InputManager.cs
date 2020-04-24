4c2799315df91b6de75bf6dcfd0970243517c1d4
weishui <kevin.feng@hotmail.com>
2020-04-24 12:12:29 +0800

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float cameraPanSpeed;
+
+    public InputMaster inputMaster;
+    Vector2 movementInput;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inputMaster = new InputMaster();
+        inputMaster.Player.Move.performed += ctx => { movementInput = ctx.ReadValue<Vector2>(); Debug.Log("moving"); };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        Camera.main.transform.Translate(movementInput * cameraPanSpeed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+}
